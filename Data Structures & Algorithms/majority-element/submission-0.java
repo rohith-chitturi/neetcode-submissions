44c1e8885a6bcb0b83a249102965049e20ec6669
Chitturi Naga Raja Teja Rohith <chitturirohith333@gmail.com>
2026-09-08 19:43:34 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        int max=Integer.MIN_VALUE;
+        int ans=0;
+        for(int i:map.keySet()){
+            if(map.get(i)>max){
+                max=map.get(i);
+                ans=i;
+            }
+        }
+        return ans;
+    }
+}
